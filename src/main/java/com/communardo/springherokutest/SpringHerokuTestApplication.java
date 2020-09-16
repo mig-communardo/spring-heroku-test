9595ac333d5261e67804289e888710805379a251
Mirjan Gjeta <you@example.com>
2020-09-16 17:26:39 +0200

    Initial Commit

---
+++ src/main/java/com/communardo/springherokutest/SpringHerokuTestApplication.java
@@ -0,0 +1,13 @@
+package com.communardo.springherokutest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHerokuTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHerokuTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/communardo/springherokutest/SpringHerokuTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.communardo.springherokutest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHerokuTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
